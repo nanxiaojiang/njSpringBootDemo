55a707f689f201776e3916c686f4305745728cd4
nanjiang <nanjiangdayd@163.com>
2021-01-04 18:32:29 +0800

    导入@EnableZuulProxy注解包

--- 06_springcloud_api_gateway/src/main/java/com/nj/springcloud/SpringCloudZuulApplication.java
+++ 06_springcloud_api_gateway/src/main/java/com/nj/springcloud/SpringCloudZuulApplication.java
@@ -2,10 +2,11 @@ package com.nj.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @author 南江
- * @Description: ${todo}
+ * @Description: 使用 Zuul 构建 API 网关使用Spring boot 入口类
  * @date 2020/12/27 11:00
  */
 @SpringBootApplication
